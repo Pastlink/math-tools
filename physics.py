20670c359c9ade986183f785e48458200ac30ae4
PastLink <pl.programming1151@gmail.com>
2024-04-10 20:07:40 -0500

    Added landscaping, simple area function

--- physics.py
+++ physics.py
@@ -2,6 +2,9 @@ from common_tools import cprint, colorize as clr, clean_string, check_if_int
 from square_root import square_root
 
 
+def landscaping(n: int) -> int | float:
+    return check_if_int(round(square_root(n), 1))
+
 def basic_grav(n: int) -> int | float:
     return check_if_int(round(square_root(n / 4), 1))
 
@@ -13,8 +16,9 @@ def print_result(data):
         cprint("No valid parameters!", "RED")
     else:
         data = int(data)
-        print(f'{clr("Gravity:", "BLUE")} {basic_grav(data)} seconds!')
-        print(f'{clr("Speed:", "GREEN")} {car_speed(data)} mph!')
+        print(f'{clr("Landscaping:", "BLUE")} {landscaping(data)} feet!')
+        print(f'{clr("Gravity:", "GREEN")} {basic_grav(data)} seconds!')
+        print(f'{clr("Speed:", "RED")} {car_speed(data)} mph!')
 
 
 def main():
