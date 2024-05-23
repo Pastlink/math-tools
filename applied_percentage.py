ddab532330e3a212df827022abf97e4feec4ec13
PastLink <pl.programming1151@gmail.com>
2024-05-23 15:50:52 -0500

    Refactor, functional version

--- applied_percentage.py
+++ applied_percentage.py
@@ -1,58 +1,53 @@
 from common_tools import cprint, is_int, get_data_list, clean_array_data
 
 
-def format_data(data):
+def format_data(data: str) -> str:
     if "percent" in data:
-        of_value = data.index("of")
-        is_value = data.index("is")
+        of_value: int = data.index("of")
+        is_value: int = data.index("is")
         total, result = tuple(clean_array_data(data))
 
         if of_value < is_value:
-            percent = is_int(round(result / total * 100, 2))
+            percent = is_int(result / total * 100, 2)
             return f"{percent}% of {total} is {result}."
         else:
-            percent = is_int(round(total / result * 100, 2))
+            percent = is_int(total / result * 100, 2)
             return f"{percent}% of {result} is {total}."
 
+    money_flag: bool = False
     for i in data:
-        percentage = "%" in i
-        money = "$" in i
-
-        # print("Looking at:", i)
+        percentage: bool = "%" in i
+        money: bool = "$" in i
 
         if percentage:
-            percent = i
-            percent_value = data.index(i)
-            decimal = is_int(round((float(i[:-1]) / 100), 4))
-            # cprint("Percent:", "GREEN", percent)
-            # cprint("Decimal:", "GREEN", decimal)
+            percent: str = i
+            percent_value: int = data.index(i)
+            decimal: float | int = is_int(float(i[:-1]) / 100, 4)
         elif money:
-            total = is_int(float(i[1:]))
-            total_value = data.index(i)
-            # cprint("Total money:", "GREEN", total)
+            total: float | int = is_int(float(i[1:]))
+            total_value: int = data.index(i)
+            money_flag = True
         elif not money and not percentage:
             try:
-                total = is_int(float(i))
-                total_value = data.index(i)
-                # cprint("Total:", "GREEN", total)
+                total: float | int = is_int(float(i))
+                total_value: int = data.index(i)
             except ValueError:
-                # cprint("Skipped.", "RED")
                 continue
 
-    if total_value > percent_value and not money:
-        # cprint("Multiply!", "YELLOW")
-        result = is_int(round(total * decimal, 2))
+    if total_value > percent_value:
+        result = is_int(total * decimal, 2)
     else:
-        # cprint("Divide!", "YELLOW")
-        # print(f"{total} / {decimal} = {total / decimal}")
-        result = is_int(round(total / decimal, 2))
+        result = is_int(total / decimal, 2)
 
-    if money:
-        return f"{percent} of ${result} is ${total}."
-    elif total_value < percent_value:
-        return f"{total} is {percent} of {result}."
+    if money_flag:
+        dlls = " $"
     else:
-        return f"{percent} of {total} is {result}."
+        dlls = " "
+
+    if total_value > percent_value:
+        return f"{percent} of{dlls}{total} is{dlls}{result}."
+    else:
+        return f"{percent} of{dlls}{result} is{dlls}{total}."
 
 
 def print_result(data):
@@ -77,24 +72,24 @@ if __name__ == "__main__":
     from tester import test_all
 
     to_test = [
-        ["6.5%", "of", "what", "number", "is", "$1.17"],
+        ["$1.95", "is", "7.5%", "of", "what", "number"],
         ["What", "number", "is", "35%", "of", "90"],
         ["125%", "of", "28", "is", "what", "number"],
         ["36", "is", "75%", "of", "what", "number"],
-        ["6.5%", "of", "what", "number", "is", "$1.17"],
+        ["$1.17", "is", "6.5%", "of", "what", "number"],
         ["What", "percent", "of", "36", "is", "9"],
         ["144", "is", "what", "percent", "of", "96"],
-        ["20%", "of", "what", "number", "is", "$16"]
+        ["$16", "is", "20%", "of", "what", "number"],
     ]
     to_expect = [
-        "6.5% of $18 is $1.17.",
+        "7.5% of $26 is $1.95.",
         "35% of 90 is 31.5.",
         "125% of 28 is 35.",
-        "36 is 75% of 48.",
+        "75% of 48 is 36.",
         "6.5% of $18 is $1.17.",
         "25% of 36 is 9.",
         "150% of 96 is 144.",
-        "20% of $80 is $16."
+        "20% of $80 is $16.",
     ]
 
     test_all(format_data, to_test, to_expect)
